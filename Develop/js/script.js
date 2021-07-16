1e54866297909eaa967d200fd00b7ef0ef1954ba
Denise <dmtuominen@gmail.com>
2021-07-15 21:17:33 -0600

    saving for the night

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,19 +1,19 @@
 // I need to add today date to the header
-
 var currentDay = moment().format("dddd, MMMM Do YYYY"); 
 var currentDayEL = $("#currentDay");
     currentDayEL.text(currentDay);
 
 //assign save button to click listener 
-
-$(document).click(saveBtn);
+// $("#saveBtn").on("click", function (){
+//    var text = $(this)=".description")
+// };
 
 // localStorage.setItem('todolist',(list));
 
-// var hour8Store =  $("#hour8 .description").text;
-// localStorage.setItem("hour8", hour8Store);
+//  var hour8Store =  $("#hour8 .description").text;
+//  localStorage.setItem("hour8", hour8Store);
 
-localStorage.content = $('#hour8').html('description');
+ localStorage.content = $('#hour8').html('description');
 
 //set items to local storage
 
@@ -27,8 +27,7 @@ localStorage.content = $('#hour8').html('description');
 
 
 //get current number of hours - form moment.js
-
-
+var currenthour = moment().hour();
 
 //create a loop over the block of time
 
